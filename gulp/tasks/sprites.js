2e37d4fd8b9994d9b33fe4897ae21d3730509357
Trev <trevor.pawlewicz@gmail.com>
2017-01-12 17:27:19 -0500

    setting up sprites with gulp pt.2

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -2,13 +2,14 @@
 
 var gulp      = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
+var rename    = require('gulp-rename');
 //----------------------------------------------------------------------------
-
 
 
 var config = {
     mode: {
         css: {
+            sprite: 'sprite.svg',
             render: {
                 css: {
                     template: './gulp/templates/sprite.css'
@@ -19,19 +20,32 @@ var config = {
 }
 
 
-
-
-
-
-
-
-
-
-
-//         name you task, it's function
+// create the sprite:
+//        name your task, task
 gulp.task('createSprite', function() {
     //
     return gulp.src('./app/assets/images/icons/**/*.svg')
             .pipe(svgSprite(config))
             .pipe(gulp.dest('./app/temp/sprite/'));
 });
+
+//
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+    //
+    return gulp.src('./app/temp/sprite/css/**/*.svg')
+            .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+
+// copy the sprite into our folder structure:
+//                            dependency (won't run until this is completed)
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+    //
+    return gulp.src('./app/temp/sprite/css/*.css')
+            .pipe(rename('_sprite.css'))
+            .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+
+// run ALL these tasks...
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
