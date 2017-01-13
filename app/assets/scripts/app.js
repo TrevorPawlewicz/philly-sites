643ce0be1f51721e7ec252a8c2554f7bb339f2a6
Trev <trevor.pawlewicz@gmail.com>
2017-01-13 18:12:02 -0500

    started MobileMenu.js class

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,1 +1,5 @@
-alert('you smell!');
+// import the class from the JS file:
+import MobileMenu from './modules/MobileMenu';
+
+// save new instance to our variable
+var mobileMenu = new MobileMenu();
---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,10 @@
+// var $ = require('jquery'); // same as ES5
+import $ from 'jquery';
+
+class MobileMenu {
+    constructor() {
+
+    }
+}
+
+export default MobileMenu;
