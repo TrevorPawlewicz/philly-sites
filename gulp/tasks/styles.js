99cb3348c12cb857eacd0b2431e6b01ca3770f89
Trev <trevor.pawlewicz@gmail.com>
2017-01-13 10:30:23 -0500

    style header pt.1

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,6 +5,7 @@ var cssvars      = require('postcss-simple-vars'); // for CSS variables
 var nested       = require('postcss-nested'); // nesting CSS
 var cssImport    = require('postcss-import');
 var mixins       = require('postcss-mixins');
+var hexrgba      = require('postcss-hexrgba'); // converts rgba to hex to use variables
 //----------------------------------------------------------------------------
 
 
@@ -19,7 +20,7 @@ var mixins       = require('postcss-mixins');
 gulp.task('styles', function(){
 
     return gulp.src('app/assets/styles/styles.css')
-            .pipe(postcss([cssImport, mixins, nested, cssvars, autoprefixer]))
+            .pipe(postcss([cssImport, mixins, nested, cssvars, hexrgba, autoprefixer]))
             .on('error', function(errorInfo) {
                 console.log(errorInfo.toString());
                 this.emit('end');
