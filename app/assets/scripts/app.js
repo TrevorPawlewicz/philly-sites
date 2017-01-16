a3a0327479d3c44858d98d278f1de56d31d01826
Trev <trevor.pawlewicz@gmail.com>
2017-01-16 14:42:51 -0500

    adding a stickheader to menu bar

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,12 @@
 // import the class from the JS file:
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
+import StickyHeader from './modules/StickyHeader.js';
 import $ from 'jquery';
 
-// save new instance to our variable
+
+// save new instance to our variables
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
