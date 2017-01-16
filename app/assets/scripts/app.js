16b0a31922dcd44d876f25708cbaf059390406ab
Trev <trevor.pawlewicz@gmail.com>
2017-01-16 10:21:16 -0500

    adding fade effect to items with waypoints package

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,7 @@
 // import the class from the JS file:
 import MobileMenu from './modules/MobileMenu.js';
+import RevealOnScroll from './modules/RevealOnScroll.js';
 
 // save new instance to our variable
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
