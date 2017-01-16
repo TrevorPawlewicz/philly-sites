fb38ebf43f0cc64052cadb9cee5dd16782c2084b
Trev <trevor.pawlewicz@gmail.com>
2017-01-16 09:20:17 -0500

    added site-header menu burger toggle animation

--- app/temp/scripts/app.js
+++ app/temp/scripts/app.js
@@ -101,6 +101,7 @@
 	            //               jQuery func
 	            this.menuContent.toggleClass("site-header__menu-content--is-visible");
 	            this.siteHeader.toggleClass("site-header--is-expanded");
+	            this.menuIcon.toggleClass("site-header__menu-icon--close-x");
 	        }
 	    }]);
 
