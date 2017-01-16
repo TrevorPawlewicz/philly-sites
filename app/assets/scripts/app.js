6fa8ad9c07afc6eff8295fafab446957b33a28be
Trev <trevor.pawlewicz@gmail.com>
2017-01-16 17:06:30 -0500

    completed modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,6 +2,7 @@
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
 import StickyHeader from './modules/StickyHeader.js';
+import Modal from './modules/Modal.js'
 import $ from 'jquery';
 
 
@@ -10,3 +11,4 @@ var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
